5f9578b1ac69e1a6ef24802566e75c2898bb0066
AiBiC-Data <whrnjsgh12@naver.com>
2023-08-12 21:45:55 +0900

    sol 13549

---
+++ week_3/13549_숨바꼭질_3/13549_조권호.py
@@ -0,0 +1,24 @@
+import collections
+
+n,k = map(int,input().split())
+visit = [0 for _ in range(100001)]
+
+def bfs(x):
+    global visit
+    q = collections.deque()
+    cnt=0
+    visit[x]=1;
+    q.append((x,cnt))
+    while q:
+        x,cnt = q.popleft()
+        if x==k:
+            return cnt
+        for i in [x-1, x+1, 2*x]:
+            if not visit[i]:
+                if i==2*x:
+                    q.append((i,cnt))
+                    visit[i]=1
+                else:
+                    q.append((i,cnt+1))
+                    visit[i]=1;
+print(bfs(n))
