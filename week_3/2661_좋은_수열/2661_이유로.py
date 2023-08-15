79e658160b21db345d33a7f3473351d3d3608ae0
이유로 <eurohand@naver.com>
2023-08-16 06:27:09 +0900

    2661 시간초과

--- week_3/2661_좋은_수열/2661_이유로.py
+++ week_3/2661_좋은_수열/2661_이유로.py
@@ -1,6 +1,6 @@
 # 2661 좋은 수열 (G4)
 # https://www.acmicpc.net/problem/2661
-# 오답
+# 시간초과
 
 import sys
 input = sys.stdin.readline
@@ -8,23 +8,28 @@ input = sys.stdin.readline
 n = int(input())
 
 def good(n):
-    if (n == 1):
-        return '1'
-    previous = good(n-1)
-   
-    for i in range(1, 4):
-        flag = True
-        if previous[-1] == str(i):
-            flag = False
-        for j in range(1, int(n/2)):
-            # print(previous[n-2-j*2:n-1-j], (previous[n-1-j:] + str(i)))
-            if (previous[n-2-j*2:n-1-j] == previous[n-1-j:] + str(i)):
-                flag = False
-        if flag:
-            return previous + str(i)
-    
-    return previous
+    digit = ['1', '2', '3']
 
-print(good(n))
+    if (n == 1):
+        return digit
+    
+    previousArr = good(n-1)
+    resultArr = []
+
+    for previous in previousArr:
+        for i in digit:
+            flag = True
+            if previous[-1] == str(i):
+                flag = False
+            for j in range(1, int(n/2)):
+                # print(previous[n-2-j*2:n-1-j], (previous[n-1-j:] + str(i)))
+                if (previous[n-2-j*2:n-1-j] == previous[n-1-j:] + str(i)):
+                    flag = False
+            if flag:
+                resultArr.append(previous + str(i))
+    
+    return resultArr
+
+print(good(n)[0])
         
 
