6a229a60c8c491420bdb90e13fc65a8a1525c6bd
rheeeuro <eurohand@naver.com>
2023-08-23 16:24:19 +0900

    2357 시간초과

---
+++ week_4/2357_최솟값과_최댓값/2357_이유로.py
@@ -0,0 +1,19 @@
+# 2357 최솟값과 최댓값 (G1)
+# https://www.acmicpc.net/problem/2357
+# 시간초과
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+arr = []
+
+for _ in range(n):
+    arr.append(int(input()))
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    print(min(arr[a-1:b]), end="")
+    print(" ", end="")
+    print(max(arr[a-1:b]))
