42dc2d48306e0a13c6be55c9331fe8541923a284
PaulLee <91371560+cebu13@users.noreply.github.com>
2023-09-01 11:38:18 +0900

    1103 성공

--- week_5/1103_게임/1103_이정호.java
+++ week_5/1103_게임/1103_이정호.java
@@ -1,44 +1,50 @@
 // 1103 게임 (G2)
 // https://www.acmicpc.net/problem/1103
-// 시간초과
+// 성공
 
 package BOJ;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main_1103_G2_게임_이정호 {
-     private static int N, M;
+    private static int N, M;
     private static char[][] graph;
+    private static int[][] dp;
     private static boolean[][] visited;
-    private static int ans = Integer.MIN_VALUE;
+    private static int tmp;
     private static int[] dx = { 0, 1, 0, -1 };
     private static int[] dy = { 1, 0, -1, 0 };
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = read();
         M = read();
         graph = new char[N][M];
+        dp = new int[N][M];
         visited = new boolean[N][M];
 
         String line;
         for (int i = 0; i < N; i++) {
             line = br.readLine();
-            for (int j = 0; j < M; j++) {
-                graph[i][j] = line.charAt(j);
-            }
+            Arrays.fill(dp[i], -1);
+            graph[i] = line.toCharArray();
         }
-        dfs(0, 0, 1);
-        System.out.println(ans);
+        System.out.println(dfs(0, 0) + 1);
     }
 
-    private static void dfs(int r, int c, int cnt) {
+    private static int dfs(int r, int c) {
         if (visited[r][c]) {
             System.out.println(-1);
             System.exit(0);
         }
+        if (dp[r][c] > -1) {
+            return dp[r][c];
+        }
         visited[r][c] = true;
+        dp[r][c] = 0;
         int nr, nc, len;
         for (int i = 0; i < 4; i++) {
             len = graph[r][c] - '0';
@@ -46,12 +52,13 @@ public class Main_1103_G2_게임_이정호 {
             nc = c + len * dy[i];
             if (nr < 0 || nr >= N || nc < 0 || nc >= M || graph[nr][nc] == 'H')
                 continue;
-            dfs(nr, nc, cnt + 1);
+            tmp = dfs(nr, nc) + 1;
+            dp[r][c] = tmp > dp[r][c] ? tmp : dp[r][c];
             visited[nr][nc] = false;
         }
-        ans = Math.max(ans, cnt);
-        return;
+        return dp[r][c];
     }
+
     static int read() throws IOException {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) > 32)
