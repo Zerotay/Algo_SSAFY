faf98c41d60febdbac9f1305e7f049d4fb0390ef
Zerotay <67823010+Zerotay@users.noreply.github.com>
2023-10-12 21:29:57 +0900

    solve 10week

--- 2313_김동건.py
+++ 2313_김동건.py
@@ -1,3 +1,5 @@
+# 실패
+
 import sys
 
 input = sys.stdin.readline
@@ -5,38 +7,38 @@ n = int(input())
 for _ in range(n):
     l = int(input())
     length = l
-    maxLst = []
     lst = [*map(int, input().split())]
-    dp = [[0,0,0] for _ in range(l)]
-    dp[0] = [lst[0],0,0]
+    dp = [[0, 0, 0] for _ in range(l)]
+    dp[0] = [lst[0], 0, 0]
     maxval = 0
+    left = right = 0
+    start = end = 0
+    sumlst = [0] * l
+    sumlst[0] = lst[0]
     for i in range(1, l):
+        # if lst[i] > 0:
+        #     tmp = dp[i - 1] + lst[i]
+        #     end = i
+        # else:
+        #     tmp = dp[i - 1]
+        # dp[i] = tmp
 
-        if lst[i] > 0:
-            tmp = dp[i-1]+lst[i]
+        sumlst[i] = sumlst[i - 1] + lst[i]
+        if sumlst[i] > 0:
             end = i
-        else:
-            tmp = dp[i-1]
-        dp[i] = tmp
+            if sumlst[i] > maxval:
+                maxval = sumlst[i]
+                left = start
+                right = end
+        elif sumlst[i] == 0:
 
-
-        dp[i] = dp[i-1]+lst[i]
-        if dp[i] > maxval:
-            maxval = dp[i]
-
-
-
+        if sumlst[i] < 0:
+            start = -1
+            end = i
 
 
 
-    maxval = max(sumlst)
-    for i in range(l):
-        if sumlst[i] == maxval:
-            maxLst.append(i)
-    for i in reversed(maxLst):
-
-
-    print(sumlst)
+print(sumlst)
 
 # dp를 쓰자
 # 각 위치를 끝점으로 하는 가장 큰 값 찾기
