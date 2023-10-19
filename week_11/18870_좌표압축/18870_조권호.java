cb6ab46b29e59f1be983086ad939df82231ef8a7
kwonho <76275193+AiBiC-Data@users.noreply.github.com>
2023-10-19 09:06:33 +0900

    sol 18870

--- week_11/18870_좌표압축/18870_조권호.java
+++ week_11/18870_좌표압축/18870_조권호.java
@@ -1,9 +1,8 @@
-import java.io.BufferedReader;
 // 18870 좌표압축 (S2)
 // https://www.acmicpc.net/problem/18870
 // 정답
 
-
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
