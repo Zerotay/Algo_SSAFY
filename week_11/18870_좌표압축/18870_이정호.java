dca105ae7540104c2dcc12e8bccbc759570b209b
PaulLee <91371560+cebu13@users.noreply.github.com>
2023-10-20 02:56:29 +0900

    18870 sol

--- week_11/18870_좌표압축/18870_이정호.java
+++ week_11/18870_좌표압축/18870_이정호.java
@@ -1,7 +1,6 @@
 // 18870 좌표압축 (S2)
 // https://www.acmicpc.net/problem/18870
-// 실패
-
+// 성공
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,16 +9,30 @@ import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-	  StringTokenizer st;
-	  StringBuilder sb = new StringBuilder();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] nums = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			nums[i] = Integer.parseInt(st.nextToken());
-		}
-    
+  public static void main(String[] args) throws IOException {
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    HashMap<Integer, Integer> rankMap = new HashMap<Integer, Integer>();
+
+    int[] org = new int[N];
+    int[] sorted = new int[N];
+    st = new StringTokenizer(br.readLine(), " ");
+    for (int i = 0; i < N; i++) {
+      org[i] = sorted[i] = Integer.parseInt(st.nextToken());
+    }
+    Arrays.sort(sorted);
+    int rank = 0;
+    for (int i : sorted) {
+      if (!rankMap.containsKey(i)) {
+        rankMap.put(i, rank);
+        rank++;
+      }
+    }
+    for (int i : org) {
+      sb.append(rankMap.get(i)).append(" ");
+    }
+    System.out.println(sb);
   }
 }
