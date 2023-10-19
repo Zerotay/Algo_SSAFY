244e5b0240ba7e7642c6ee8a5d6ed0a27de194a0
kwonho <76275193+AiBiC-Data@users.noreply.github.com>
2023-10-19 09:05:06 +0900

    sol 18870

---
+++ week_11/18870_좌표압축/18870_조권호.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		HashMap<Integer, Integer> hmap = new HashMap<>();
+		int[] nums = new int[N];
+		int[] puts = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			puts[i] = nums[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(nums);
+
+		for (int i = 0, idx = 0; i < N; i++) {
+			if (hmap.containsKey(nums[i]))
+				continue;
+			hmap.put(nums[i], idx++);
+		}
+
+		for (int i = 0; i < N; i++) {
+			sb.append(hmap.get(puts[i]) + " ");
+		}
+		System.out.println(sb.toString());
+
+	}
+}
