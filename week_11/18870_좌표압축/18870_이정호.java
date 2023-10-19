f459f0c68732887270e1cccdbea61656777d8d55
PaulLee <91371560+cebu13@users.noreply.github.com>
2023-10-20 02:36:47 +0900

    18870_failed

---
+++ week_11/18870_좌표압축/18870_이정호.java
@@ -0,0 +1,25 @@
+// 18870 좌표압축 (S2)
+// https://www.acmicpc.net/problem/18870
+// 실패
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	  StringTokenizer st;
+	  StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] nums = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+    
+  }
+}
